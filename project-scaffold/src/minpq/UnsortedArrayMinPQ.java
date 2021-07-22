5ec14eb31627a3b9f07b83a847168736bc3fa738
MAGICPEA <MAGICPEA@UW.EDU>
2021-07-22 15:42:39 -0700

    wtf is going on I wanna drop out

--- project-scaffold/src/minpq/UnsortedArrayMinPQ.java
+++ project-scaffold/src/minpq/UnsortedArrayMinPQ.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import org.graalvm.compiler.nodes.ProxyNode;
 
 /**
  * Unsorted array (or {@link ArrayList}) implementation of the {@link ExtrinsicMinPQ} interface.
