e77fe76e26bd4daca77734697043f59e678ef568
Dylan Renard <75507784+drenard921@users.noreply.github.com>
2021-08-10 18:28:53 -0700

    REE

--- project-scaffold/src/seamcarving/GenerativeSeamFinder.java
+++ project-scaffold/src/seamcarving/GenerativeSeamFinder.java
@@ -67,11 +67,15 @@ public class GenerativeSeamFinder implements SeamFinder {
         private final Node source = new Node() {
             @Override
             // YES WE ALSO NEED TO DO THIS METHOD
+            // Giga chad whole grain grind set quinoa warrior
             public List<Edge<Node>> neighbors(Picture picture, EnergyFunction f) {
+<<<<<<< HEAD
                 Pixel[][] pixels = new Pixel[picture.width()][picture.height()];
+=======
+>>>>>>> parent of d08655a (Revert "reee")
                 List<Edge<Node>> result = new ArrayList<>(picture.height());
                 for (int j = 0; j < picture.height(); j += 1) {
-                    Pixel to = pixels[0][j];
+                    Pixel to = new Pixel(0, j);
                     result.add(new Edge<>(source, to, f.apply(picture, 0, j)));
                 }
                 System.out.println("Result is this big : " + result.size());
@@ -105,8 +109,23 @@ public class GenerativeSeamFinder implements SeamFinder {
         // DO NOT OVERRIDE THIS METHOD
         @Override
         public List<Edge<Node>> neighbors(Node node) {
+<<<<<<< HEAD
             Pixel from = new Pixel(0, 0);
             return from.neighbors(picture, f);
+=======
+            List<Edge<Node>> result = new ArrayList<>();
+            // essentially  this one is looking up, looking forward, looking down from a given node which contains this info
+            System.out.println(node);
+
+//            for (int z = y - 1; z <= y + 1; z += 1)
+//                // Only if the neighbor is in the bounds of the picture.
+//                if (0 <= z && z < picture.height()) {
+//                    result.add(new Edge<>(from, to, f.apply(picture, x + 1, z)));
+//                }
+//            }
+            return result;
+            // return node.neighbors(picture, f);
+>>>>>>> parent of d08655a (Revert "reee")
         }
 
         /**
@@ -139,6 +158,7 @@ public class GenerativeSeamFinder implements SeamFinder {
                 Pixel[][] pixels = new Pixel[picture.width()][picture.height()];
                 List<Edge<Node>> result = new ArrayList<>(picture.height());
                 // Starting from the rightmost column, each pixel has only a single edge to the sink (with 0 weight).
+<<<<<<< HEAD
                 for (int y = 0; y < picture.height(); y += 1) {
                     Pixel from = new Pixel(picture.width() - 1, y);
                     pixels[picture.width() - 1][y] = from;
@@ -161,6 +181,56 @@ public class GenerativeSeamFinder implements SeamFinder {
                     }
                 }
                 return result;
+=======
+//                for (int y = 0; y < picture.height(); y += 1) {
+//                    Pixel from = new Pixel(picture.width() - 1, y);
+//                    // pixels[picture.width() - 1][y] = from;
+//                    result.add(new Edge<>(from, sink, 0));
+//                }
+//                // Starting from the next-rightmost column...
+//                for (int x = picture.width() - 2; x >= 0; x -= 1) {
+//                    // Consider each pixel in the column...
+//                    for (int y = 0; y < picture.height(); y += 1) {
+//                        Pixel from = new Pixel(x, y);
+//                        // pixels[x][y] = from;
+//                        // Connect the pixel to its right-up, right-middle, and right-down neighbors...
+//                        for (int z = y - 1; z <= y + 1; z += 1) {
+//                            // Only if the neighbor is in the bounds of the picture.
+//                            if (0 <= z && z < picture.height()) {
+//                                Pixel to = pixels[x + 1][z];
+//                                result.add(new Edge<>(from, to, f.apply(picture, x + 1, z)));
+//                            }
+//                        }
+//                    }
+//                }
+//                return result;
+
+
+                // seam finder gives the list of pixels you want to find neighbors for
+
+                // we need to call neighbors / implement
+
+                // to add the neighbors of each pixel in that original seamfinder
+
+                // iterate over seamfinder call
+
+                // if statement from pixel
+
+                // add them
+
+//                for (int z = y - 1; z <= y + 1; z += 1) {
+//                    // Only if the neighbor is in the bounds of the picture.
+//                    if (0 <= z && z < picture.height()) {
+//                        AdjacencyListSeamFinder.PixelGraph.Pixel to = pixels[x + 1][z];
+//                        from.neighbors.add(new Edge<>(from, to, f.apply(picture, x + 1, z)));
+//                        System.out.println("Neighbors : " + from.neighbors);
+//                    }
+//                }
+
+
+                return result;
+
+>>>>>>> parent of d08655a (Revert "reee")
             }
 
             @Override
