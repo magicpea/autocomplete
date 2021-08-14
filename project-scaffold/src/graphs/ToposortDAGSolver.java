6614b98b3cb6188c06f3e9ca45f78cb718ade522
MAGICPEA <MAGICPEA@UW.EDU>
2021-08-14 12:23:16 -0700

    good work

--- project-scaffold/src/graphs/ToposortDAGSolver.java
+++ project-scaffold/src/graphs/ToposortDAGSolver.java
@@ -25,53 +25,49 @@ public class ToposortDAGSolver<V> implements ShortestPathSolver<V> {
         this.edgeTo = new HashMap<>();
         this.distTo = new HashMap<>();
 
-        System.out.println("Scalloped Potatoes");
+//        System.out.println("Scalloped Potatoes");
+        List <V> result = new ArrayList<>();
+        Set<V> visited = new HashSet<>();
 
-        // we took alot from DJ solver
-        // We wonder if we need to make any other key changes?
-
-        // gross unecessary structure
-        ExtrinsicMinPQ<V> pq = new DoubleMapMinPQ<>();
-
-        // Christmas tree topper node (The very first node):
-        // add the start node (with its given edgeTo ordering, and distance to this node)
-        pq.add(start, 0.0);
-        edgeTo.put(start, null);
-        distTo.put(start, 0.0);
-
-
-//        while (!pq.isEmpty()) {
-//            V from = pq.removeMin();
-//            for (Edge<V> e : graph.neighbors(from)) {
-//                V to = e.to;
-//                double oldDist = distTo.getOrDefault(to, Double.POSITIVE_INFINITY);
-//                double newDist = distTo.get(from) + e.weight;
-//                if (newDist < oldDist) {
-//                    edgeTo.put(to, e);
-//                    distTo.put(to, newDist);
-//                    if (pq.contains(to)) {
-//                        pq.changePriority(to, newDist);
-//                    } else {
-//                        pq.add(to, newDist);
-//                    }
-//                }
-//            }
-//            // comments marked for deletion later on
-////            System.out.println("edge to: " + this.edgeTo.toString());
-////            System.out.println("dist to: " + this.distTo.toString());
-//             List <V> result = new ArrayList<>();
-//             Set<V> visited = new HashSet<V>();
-//
-//             dfsPostOrder(graph, start, visited, result);
-//            System.out.println("DFS post  result: " + result);
-
-
-//        Initialize DFS post order params
-//
-//
+        dfsPostOrder(graph, start, visited, result);
+        // initializing our starting nodes
+        this.edgeTo.put(start, null);
+        this.distTo.put(start, 0.0);
+        V from = start;
+//        for(V vertex : result) {
+            // for every node, we want to update its connections with that postorder
+            // maybe this operates on a single node ?
+        int maxIndex = graph.neighbors(from).size();
+//        for (Edge<V> edge : graph.neighbors(from)) {
+        for (int i = 0; i <= graph.neighbors(from).size() - 1; i++) {
+            Edge<V> edge = graph.neighbors(from).get(i);
+                V to = edge.to;
+                System.out.println("P1");
+                double oldDist = distTo.getOrDefault(to, Double.POSITIVE_INFINITY);
+                System.out.println("P2");
+                if(i == graph.neighbors(from).size() - 1) {
+                    // we need to initialize distTo and edgeTo here
+                    System.out.println("P3");
+                    edgeTo.put(to, null);
+                    System.out.println("P4");
+                    distTo.put(to, oldDist);
+                    System.out.println("P5");
+                } else {
+                    System.out.println("P6");
+                    double newDist = distTo.get(from) + edge.weight;
+                    System.out.println("P7");
+                    // updating the shortest path
+                    if(newDist < oldDist) {
+                        System.out.println("P8");
+                        edgeTo.put(to, edge);
+                        System.out.println("P9");
+                        distTo.put(to, newDist);
+                        System.out.println("P10");
+                    }
+                }
+                System.out.println("does this break?");
         }
-
-
+        System.out.println("does THIS break?");
     }
 
     /**
@@ -84,7 +80,7 @@ public class ToposortDAGSolver<V> implements ShortestPathSolver<V> {
      */
     private void dfsPostOrder(Graph<V> graph, V start, Set<V> visited, List<V> result) {
         // comment marked for deletion later
-        System.out.println("we in this joint");
+//        System.out.println("we in this joint");
 
         // we know we need to call collections.reverse at some point
 
@@ -94,39 +90,37 @@ public class ToposortDAGSolver<V> implements ShortestPathSolver<V> {
 
         // stores the set of discovered vertices so we don't revisit them multiple times
 //        Set<V> discovered = new Set<>();
-
         // kicking off our starting point by adding it to the perimeter
         perimeter.add(start);
         while (!perimeter.isEmpty()) {
             V from = perimeter.remove(0);
+//            System.out.println(from.toString());
             if (!visited.contains(from)) {
                 for (Edge<V> bro : graph.neighbors(from)) {
                     V to = bro.to;
                     perimeter.add(to);
                     // we added this try it out
-                    result.add(to);
+//                    result.add(to);
+//                    visited.add(from);
+//                    result.add(from);
                 }
                 visited.add(from);
+                result.add(from);
             }
         }
-
+//        System.out.println("Visited nodes: " + visited);
+//        System.out.println("Result nodes: " + result);
+//        System.out.println("Before: " + result);
+        Collections.reverse(result);
+//        System.out.println("After: " + result);
         // Debug code for deletion later
 //        System.out.println("Visited: " + visited.toString());
-
-
         // 2. Return verticies in reverse DFS post-order
-
         // essentially take DFS and reverse at the end
-
         // Questions:
-
-        // Why/Where are we calling DFS post order?
-
         // How do we implement toposort objective 2 and 3
-
         // Is it just by building our DFS list of results?
         // then calling Collections.reverse
-
     }
 
     @Override
