00c10f9610befea0a6fc4245e23288e5b8979991
MAGICPEA <MAGICPEA@UW.EDU>
2021-08-15 12:39:36 -0700

    let's goooooo

--- project-scaffold/src/seamcarving/DynamicProgrammingSeamFinder.java
+++ project-scaffold/src/seamcarving/DynamicProgrammingSeamFinder.java
@@ -14,20 +14,14 @@ import java.util.Map;
  * @see SeamCarver
  */
 public class DynamicProgrammingSeamFinder implements SeamFinder {
-
-
     @Override
     public List<Integer> findSeam(Picture picture, EnergyFunction f) {
         // Generative find seam:
-
         // Initialization of our data structures:
-
         // initialize a 2D array for the picture (DONE YAY)
         double pixels[][] = new double[picture.width()][picture.height()];
-
         // what we return to the user as the key value pairs for the best pixel in each given x coordinate
         // we treated the first starting point from column 0
-
         // as the pixel with the lowest energy value in column 0.
         // we want to build our first column of energy values
         // also finding the minimum energy from that column and adding it to our result
@@ -35,9 +29,7 @@ public class DynamicProgrammingSeamFinder implements SeamFinder {
 
         // left most pixels: (initial edge case)
         double min_energy = 9;
-
         // we treated the first starting point from column 0
-
         // as the pixel with the lowest energy value in column 0.
         // we want to build our first column of energy values
         // also finding the minimum energy from that column and adding it to our result
@@ -52,24 +44,27 @@ public class DynamicProgrammingSeamFinder implements SeamFinder {
 
             }
         }
-
+        for(int x = 1; x < picture.width(); x++) {
+            for(int y = 0; y < picture.height(); y++) {
+                pixels[x][y] = f.apply(picture, x, y);
+                // iterate over neighbors and figure out the min
+            }
+        }
         // now we need to start finding neighbors from our best first source node
-
+//        if (x == picture.width() -1) {
+//            return List.of( new Edge<>(this, sink, 0));
+//        }
         // essentially a for loop till we reach the edge of the picture
 
         // checking neighbors of our pixel
 
         // also we need to address how we calculate the best neighbor to go to
 
-
-
         // we treated the first starting point from column 0
 
         // as the pixel with the lowest energy value in column 0.
         // we want to build our first column of energy values
         // also finding the minimum energy from that column and adding it to our result
-
-
         // iterate over middle of picture (large for loop (x to picture width -2))
         // apply energy function
         // look for best neighbor (left up, left middle, left down) (private neighbors methods)
@@ -79,17 +74,35 @@ public class DynamicProgrammingSeamFinder implements SeamFinder {
         // figure out how to do this
 
         // the best seam we want to return back
-
-
-
         ///
         List<Integer> result = new ArrayList<>();
-
-
-
-
         return result;
+    }
 
+    // private helper method, basically just returns the neighbors of a given pixel
+    // now, we need to figure out how to return the best neighbor !
+    // returns the energy value of min neighbor
 
+    private double neighbors(Picture picture, EnergyFunction f, int x, int y) {
+        Map<Integer, Integer> neighborList = new HashMap<>(3);
+        double min = -1;
+        // if we are out of bounds horizontally
+        if (x == picture.width() - 1) {
+            return  min;
+        }
+        for (int z = y - 1; z <= y + 1; z += 1) {
+            // Only if the neighbor is in the bounds of the picture.
+            if (0 <= z && z < picture.height()) {
+                // wtf is this line doing ?
+                neighborList.put(x + 1, z);
+            }
+        }
+        for(Integer key : neighborList.keySet()) {
+            double curr = f.apply(picture, key, neighborList.get(key));
+            if(curr < min || min == -1) {
+                min = curr;
+            }
+        }
+        return min;
     }
 }
