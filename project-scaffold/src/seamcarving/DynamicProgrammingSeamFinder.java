14a59e15bf407b250798b11799104d0a6e9e239e
Dylan Renard <75507784+drenard921@users.noreply.github.com>
2021-08-16 10:30:34 -0700

    ree

--- project-scaffold/src/seamcarving/DynamicProgrammingSeamFinder.java
+++ project-scaffold/src/seamcarving/DynamicProgrammingSeamFinder.java
@@ -43,7 +43,8 @@ public class DynamicProgrammingSeamFinder implements SeamFinder {
 
 
         int x_i = picture.width() - 1;
-        for (int x = x_i; x >= 0; x++) {
+
+        for (int x = x_i; x >= 0; x--) {
             // x is zero test case
             if (x == 0){
                 for (int y = 0; y < picture.height(); y++) {
@@ -52,16 +53,20 @@ public class DynamicProgrammingSeamFinder implements SeamFinder {
                 }
             }
             // width to 1
+            List appendix = new ArrayList<Double>();
             for (int y = 0; y < picture.height(); y++) {
-                ArrayList appendix = neighbors(picture, f, x, y);
+                appendix = neighbors(picture, f, x, y);
+//                System.out.println(appendix.toString());
                 lobotomy.add(appendix);
                 // now we need to apply the energy function
                 // R -> L
                 // start from right then add outer for loop till we reach left
                 // call neighbors
             }
-            seam.add(updateEnergy(x, pixels, lobotomy));
+            seam.add(updateEnergy(x,  pixels, lobotomy));
         }
+
+        return seam;
 //        // iterate through the lobotomy (remember that size doesn't matter ;P )
 //        // min value
 //        double minVal = -1.0;
@@ -99,59 +104,92 @@ public class DynamicProgrammingSeamFinder implements SeamFinder {
         // the best seam we want to return back
 
 //        return mf_coordinates;
-        return seam;
     }
 
     // private helper method, basically just returns the neighbors of a given pixel
     // now, we need to figure out how to return the best neighbor !
     // returns the energy value of min neighbor
 
-    private ArrayList neighbors(Picture picture, EnergyFunction f, int x, int y) {
+    private List<Double> neighbors(Picture picture, EnergyFunction f, int x, int y) {
         Map<Integer, Integer> neighborList = new HashMap<>(3);
-        ArrayList concussion = new ArrayList<Double>(2);
+        List concussion = new ArrayList<Double>(2);
         double min = - 1;
-        int best_y = - 1;
+        int best_y = -1;
 
         // if we are out of bounds horizontally
-        if (x == picture.width() - 1) {
+        if (x <= 0) {
             return concussion;
         }
         for (int z = y - 1; z <= y + 1; z += 1) {
             // Only if the neighbor is in the bounds of the picture.
             if (0 <= z && z < picture.height()) {
                 // wtf is this line doing ?
-                neighborList.put(x + 1, z);
+                neighborList.put(x - 1, z);
             }
         }
         double parent_e_val = f.apply(picture, x, y);
         for(Integer key : neighborList.keySet()) {
             // current energy value
-            double neighbor_path_e_value = f.apply(picture, key, neighborList.get(key));
-            double e_sum = neighbor_path_e_value + parent_e_val;
+            double e_sum = parent_e_val;
+            if (key - 1 >= 0) {
+                double neighbor_path_e_value = f.apply(picture, key, neighborList.get(key));
+                e_sum = neighbor_path_e_value + parent_e_val;
+            } else {
+                if (min == -1) {
+                    min = e_sum;
+                    best_y = neighborList.get(key);
+                }
+            }
+//            double neighbor_path_e_value = f.apply(picture, key, neighborList.get(key));
+//            double e_sum = neighbor_path_e_value + parent_e_val;
+//            double e_sum = parent_e_val;
 
-            if(e_sum < min || min == -1) {
+
+
+            if(e_sum < min) {
                 min = e_sum;
                 best_y = neighborList.get(key);
-                concussion.add(min);
-                concussion.add((double)best_y);
             }
         }
+        // after finding best finally add them to be sent out
+        concussion.add(min);
+        concussion.add((double)best_y);
+
         return concussion;
     }
 
     private int updateEnergy(int x, double pixels[][], ArrayList<ArrayList<Double>> lobotomy) {
         // iterate through the lobotomy (remember that size doesn't matter ;P )
         // min value
+        if (x == 0) {
+            System.out.println("dammit we made a goof");
+            return -1;
+        }
+
+
         double minVal = -1.0;
         // y-value
-        int newY = -1;
+        int newY = 0;
         // looking for min energy function in the list
+
+        ArrayList temp = new ArrayList<Double>(2);
         for(int i = 0; i < lobotomy.size(); i++) {
-            ArrayList temp = lobotomy.get(i);
-            if((double)temp.get(0) < minVal) {
-                minVal = (double)temp.get(0);
-                newY = (int)temp.get(1);
-                // gotta figure out where to get the x from
+
+            temp = (ArrayList) lobotomy.get(i).clone();
+            if (temp.isEmpty()) {
+                System.out.println("we are empty at " + i + " index");
+                System.out.println("x " + x);
+            }
+
+            double curr_e = (double) temp.get(0);
+            double curr_y = (double) temp.get(1);
+            if(minVal == -1) {
+                minVal = curr_e;
+            }
+
+            if (curr_e < minVal) {
+                minVal = (double) curr_e;
+                newY = (int) curr_y;
                 pixels[x][newY] = minVal;
             }
         }
